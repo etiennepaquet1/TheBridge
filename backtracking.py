4a7d71f70f2cd16e697261136bb8fa3088fb2a6a
Etienne <adgarbage88@gmail.com>
2023-05-14 19:55:28 -0400

    2

--- backtracking.py
+++ backtracking.py
@@ -1,4 +1,5 @@
 import math
+import copy
 
 
 class Bike:
@@ -7,6 +8,13 @@ class Bike:
         self.x = x  # horizontal position
         self.y = y  # lane of bike
         self.j = j  # how much airtime before landing (0 means it's on the ground)
+
+
+class TestRunResult:
+    def __init__(self, bikesSaved: int, groundCovered: int):
+        self.bikesSaved = bikesSaved
+        self.groundCovered = groundCovered
+
 
 
 def SPEED(bikes):
@@ -50,21 +58,22 @@ def doAction(n: int, bikes: [Bike]):
         case 0:
             SPEED(bikes)
         case 1:
-            SLOW(bikes)
-        case 2:
             JUMP(bikes)
-        case 3:
-            WAIT(bikes)
-        case 4:
-            DOWN(bikes)
-        case 5:
+        case 2:
             UP(bikes)
+        case 3:
+            DOWN(bikes)
+        case 4:
+            SLOW(bikes)
+        case 5:
+            WAIT(bikes)
+
 
 
 def simulateTurn(bikes):
     bikesToRemove = []
     for bike in bikes:
-        for pos in range(bike.x, min(bike.x + bike.v + 1, len(lanes[0]) - 1)):  # range is exlusive at the upper bound so we need to add 1
+        for pos in range(bike.x, min(bike.x + bike.v, len(lanes[0]) - 1)):  # range is exclusive at the upper bound so we need to add 1
             bike.x += 1
             if bike.j > 0:
                 bike.j -= 1
@@ -73,6 +82,13 @@ def simulateTurn(bikes):
                 break
     for bike in bikesToRemove:
         bikes.remove(bike)
+
+
+    # # return the length of ground traveled
+    # if len(bikes) == 0:
+    #     return 0
+    # else:
+    #     return bikes[0].x
 
 
 def printLanesWithBikes():
@@ -88,13 +104,22 @@ def printLanesWithBikes():
 
 # takes as input the action order presented and returns the number of bikes left after the actions are taken
 def testActionOrder(actions: [int]):
+    global realBikes
     print(f"Testing actions: {actions}")
-    testBikes = realBikes.copy()
+    testBikes = copy.deepcopy(realBikes)
     for action in actions:
         doAction(action, testBikes)
         simulateTurn(testBikes)
-    print(f"{len(testBikes)} bikes are still around")
-    return len(testBikes)
+
+    groundCovered = testBikes[0].x if len(testBikes) > 0 else 0
+
+    print(f"{len(testBikes)} bikes are still around and has covered a distance of {groundCovered} ")
+
+
+    trr = TestRunResult(len(testBikes), groundCovered)
+
+    return trr  # returns a testRunResult with the number of bikes and the ground covered
+
 
 
 def incrementActionOrder(nextNActions: [int]):
@@ -152,17 +177,24 @@ nextNActions = 5 * [0]
 bestActionOrder = nextNActions
 
 mostBikesSaved = 0
-
+mostGroundCovered = 0
 
 while not finishedTesting:
+    result = testActionOrder(nextNActions)
 
-    numberSaved = testActionOrder(nextNActions)
-    if numberSaved > mostBikesSaved:
+    numberSaved = result.bikesSaved
+    groundCovered = result.groundCovered
+
+    if numberSaved > mostBikesSaved or (numberSaved == mostBikesSaved and groundCovered > mostGroundCovered):
         bestActionOrder = nextNActions
         mostBikesSaved = numberSaved
-    if numberSaved == len(realBikes):
-        break
+        mostGroundCovered = groundCovered
     incrementActionOrder(nextNActions)
+
+
+# n = testActionOrder([3, 3, 3, 3, 3])
+
+
 print(f"Best action order: {bestActionOrder}")
 
 
